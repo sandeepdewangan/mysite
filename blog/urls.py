cd4d41c6733d078f16cf814df5ebc2a3827b312e
Sandeep <sandeepdewangan7@gmail.com>
2025-08-23 07:52:02 +0530

    Class Based Views

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .import views
+from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path("", views.PostListView.as_view(), name="post_list"),
+    # path("", views.post_list, name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
+        views.post_detail,
+        name="post_detail",
+    ),
     # if path converters like <int:id>/ is not sufficient,
     # we can use re_path() for complex URL patterns with Regex.
 ]
